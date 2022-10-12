71296eb721f70f6df32fbbe582bcd4f06352996a
Lucas2155 <lucaslinhares980@gmail.com>
2022-10-11 22:08:52 -0300

    Add Docker

--- pass-generator/getAPI.js
+++ pass-generator/getAPI.js
@@ -1,5 +1,5 @@
 $("button").click(function(){
-    $.get("http://localhost:3030/getNumber", function(data, status){
+    $.get("http://ec2-44-204-193-8.compute-1.amazonaws.com:3000/getNumber", function(data, status){
       console.log(data)
     });
   });
